5c063cdcc6508f38cb0c6b38fda03efbb28fd6d6
Donytomy <83833818+Donytomy@users.noreply.github.com>
2023-06-18 14:32:59 +0530

    Dony

--- server.js
+++ server.js
@@ -1,196 +1,398 @@
+//SING UP FORM
+
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
-const User = require('./users');
-
+const nodemailer = require('nodemailer');
+const cors =require('cors');
 const app = express();
-const port = 3000;
-
-// Cloudinary configuration
-cloudinary.config({
-  cloud_name: "djrhsrorn",
-  api_key: "449643457296112",
-  api_secret: "QD0dFwLZ8QbI7aXgx_e8ClbbzOw"
-});
-
-// Connection URL and database name
-const url = 'mongodb+srv://jibbinjacob:jibbin2002@cluster0.gq0orgc.mongodb.net';
-const dbName = 'esell2024'; // Replace with your actual database name
-
+const port = 8000;
+const bodyParser = require('body-parser');
+const async = require('hbs/lib/async');
 // Connect to MongoDB
-mongoose.connect(`${url}/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://jibbinjacob:jibbin2002@cluster0.gq0orgc.mongodb.net/esell2024?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB successfully');
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
 
-// Define a schema for the product
-const productSchema = new mongoose.Schema({
-  name: String,
-  category: String,
-  description: String,
-  quantity: Number,
-  price: Number,
-  currentBid: Number,
-  date: Date,
-  time: String,
-  images: [String], // Store an array of image URLs
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+// Define a schema for the user collection
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  isDisabled:{
+    type:Boolean
+  },
+  address: {
+    streetAddress1: {
+      type: String,
+      required: true
+    },
+    streetAddress2: {
+      type: String
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    },
+    pincode: {
+      type: String,
+      required: true
+    }
+  },
+  bankDetails: {
+    accountNumber: {
+      type: String,
+      required: true
+    },
+    ifscCode: {
+      type: String,
+      required: true
+    },
+    upiId: {
+      type: String,
+      required: true
+    }
   }
 });
 
-// Define a model for the product using the schema
-const Product = mongoose.model('Product', productSchema);
 
-// Middleware
+// Create a user model based on the schema
+const User = mongoose.model('User', userSchema);
+
+
+
+app.use(cors(
+  {
+    origin: '*'
+  }
+))
+app.use(express.json());
+app.use(express.static(__dirname + '/forgotpass')); 
+app.use(express.urlencoded({ extended: true }));
+// Create a route for handling the signup form submission
+app.post('/signup', (req, res) => {
+  // Extract the form data from the request
+  const { username, password, email, phonenumber, firstname, lastname, isDisabled, addressLine1, addressLine2, accountNumber, ifscCode, upiId, state, Country, pincode, city } = req.body;
+  const bankDetails = {
+    accountNumber,
+    ifscCode,
+    upiId
+  }
+  const address = {
+    streetAddress1: addressLine1,
+    streetAddress2: addressLine2,
+    country: Country,
+    state,
+    pincode,
+    city,
+  }
+  // Create a new user instance
+  const newUser = new User({
+    username,
+    password,
+    email,
+    phoneNumber: phonenumber,
+    firstName: firstname,
+    lastName: lastname,
+    isDisabled,
+    address,
+    bankDetails,
+  });
+
+  console.log(newUser);
+
+
+   //Save the user to the database
+  User.create(newUser)
+   .then(() => {
+    res.send('User created successfully');
+  })
+ .catch((error) => {
+     console.error('Error creating user:', error);
+    res.status(500).json({ error: 'Error creating user' });
+   });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+
+
+// LoGIN FORM
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username, password });
+    if (user) {
+      // Successful login
+      res.json({ message: 'Login successful' });
+    } else {
+      // Invalid credentials
+      res.status(401).json({ error: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
+
+// //UPDATE PROFILE
+
+
+// app.use(bodyParser.json());
+
+// // Serve static files (HTML, CSS, images)
+// app.use(express.static('public'));
+
+// // Route to handle the update profile request
+// app.post('/update-profile', (req, res) => {
+//   const formData = req.body;
+
+//   // Find the user in the database and update their details
+//   User.findOneAndUpdate({}, formData, { new: true })
+//     .then(updatedUser => {
+//       if (updatedUser) {
+//         console.log('User details updated:', updatedUser);
+//         res.json({ message: 'User details updated successfully' });
+//       } else {
+//         console.log('User not found');
+//         res.status(404).json({ message: 'User not found' });
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error updating user details:', error);
+//       res.status(500).json({ message: 'Error updating user details' });
+//     });
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
+
+
+//LOGIN 
+
+
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Serve static files (HTML, CSS, images)
+app.use(express.static('public'));
+
+// Route to handle user login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Find the user in the database
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        // Compare the provided password with the stored hashed password
+        bcrypt.compare(password, user.password)
+          .then(match => {
+            if (match) {
+              // Passwords match, user is authenticated
+              res.json({ message: 'Login successful' });
+            } else {
+              // Passwords don't match, authentication failed
+              res.status(401).json({ message: 'Invalid username or password' });
+            }
+          });
+      } else {
+        // User not found
+        res.status(404).json({ message: 'User not found' });
+      }
+    })
+    .catch(error => {
+      console.error('Error finding user:', error);
+      res.status(500).json({ message: 'Error finding user' });
+    });
+});
+
+//PROFILE UPDATE
+
+// Route to handle the update profile request
+
+
+// Use body-parser middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
 
-// Multer storage configuration for Cloudinary
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'products',
-    allowed_formats: ['jpg', 'jpeg', 'png']
-  }
-});
-
-// Multer upload instance
-const upload = multer({ storage: storage });
-
-// Define the route for form submission
-app.post('/submit-form', upload.array('images', 5), async (req, res) => {
+// Set up routes
+app.post('/update-profile', (req, res) => {
   // Get the form data from the request body
-  const { name, category, description, quantity, price, currentBid, date, time } = req.body;
-  const images = req.files.map(file => file.path); // Store an array of Cloudinary URL paths
+  const { phoneNumber, addressLine1, addressLine2, city, state, pincode, accountNumber, ifscCode, upiId } = req.body;
 
-  try {
-    // Find the user in the MongoDB collection based on the email
-    const user = await User.findOne({ email: req.user.email });
+  // Find the user by their unique identifier (e.g., user ID) and update their profile
+  User.findOneAndUpdate(
+    {username: 'llllllll' }, // Replace 'USER_ID' with the actual user's ID
+    { phoneNumber, addressLine1, addressLine2, city, state, pincode, accountNumber, ifscCode, upiId },
+    { new: true } // Return the updated document
+  )
+    .then((updatedUser) => {
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      return res.json({ message: 'Profile updated successfully', user: updatedUser });
+    })
+    .catch((error) => {
+      console.error('Error updating profile:', error);
+      return res.status(500).json({ message: 'Failed to update profile' });
+    });
+});
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
 
-    // Create a new product instance
-    const product = new Product({
-      name,
-      category,
-      description,
-      quantity,
-      price,
-      currentBid,
-      date,
-      time,
-      images,
-      user: user._id // Assign the user ID to the product
+
+
+
+//forgot password
+
+const Mailgen = require('mailgen');
+
+const { EMAIL, PASSWORD } = require('./env')
+
+/** send mail from testing account */
+const signup = async (req, res) => {
+
+    /** testing account */
+    let testAccount = await nodemailer.createTestAccount();
+
+      // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
     });
 
-    // Save the product to the database
-    product.save()
-      .then(() => {
-        console.log('Product saved successfully');
-        res.sendStatus(200);
-      })
-      .catch((error) => {
-        console.error('Error saving product:', error);
-        res.status(500).send('Error saving product to the database');
-      });
-  } catch (error) {
-    console.error('Error fetching user details:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Rest of your code...
+    let message = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Successfully Register with us.", // plain text body
+        html: "<b>Successfully Register with us.</b>", // html body
+      }
 
 
+    transporter.sendMail(message).then((info) => {
+        return res.status(201)
+        .json({ 
+            msg: "you should receive an email",
+            info : info.messageId,
+            preview: nodemailer.getTestMessageUrl(info)
+        })
+    }).catch(error => {
+        return res.status(500).json({ error })
+    })
 
+    // res.status(201).json("Signup Successfully...!");
+}
 
-// ...Rest of the code remains the same...
+/** send mail from real gmail account */
+const getbill = (req, res) => {
 
+    const { userEmail } = req.body;
 
-////////////////////--display pro---////////////////////////////////////
-// Set up route to retrieve product details
-// Set up route to retrieve product details
-app.get('/products', async (req, res) => {
-  try {
-    const products = await Product.find().exec();
-    res.json(products);
-  } catch (error) {
-    console.error('Error retrieving product details from MongoDB:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-// Fetch a single product by ID
-app.get('/products/:id', async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const product = await Product.findById(productId);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ error: 'Product not found' });
+    let config = {
+        service : 'gmail',
+        auth : {
+            user: EMAIL,
+            pass: PASSWORD
+        }
     }
-  } catch (error) {
-    console.error('Error fetching product details:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
-/////////// pro details display///////////////////
-app.post('/update-bid', async (req, res) => {
-  const { productId, bidAmount } = req.body;
+    let transporter = nodemailer.createTransport(config);
 
-  if (!productId || !bidAmount) {
-    return res.status(400).json({ error: 'Invalid input' });
-  }
+    let MailGenerator = new Mailgen({
+        theme: "default",
+        product : {
+            name: "Mailgen",
+            link : 'https://mailgen.js/'
+        }
+    })
 
-  try {
-    // Update the currentBid value in the database
-    const updatedProduct = await Product.findByIdAndUpdate(productId, { currentBid: bidAmount }, { new: true });
-    if (!updatedProduct) {
-      return res.status(404).json({ error: 'Product not found' });
+    let response = {
+        body: {
+            name : "Daily Tuition",
+            intro: "Your bill has arrived!",
+            table : {
+                data : [
+                    {
+                        item : "Nodemailer Stack Book",
+                        description: "A Backend application",
+                        price : "$10.99",
+                    }
+                ]
+            },
+            outro: "Looking forward to do more business"
+        }
     }
-    return res.status(200).json({ message: 'Bid updated successfully', product: updatedProduct });
-  } catch (error) {
-    console.error('Error updating bid:', error);
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-});
-// Set the view engine to EJS
-app.set('view engine', 'ejs');
 
-// Define the route for generating multiple pages
-app.get('/generate-pages', async (req, res) => {
-  try {
-    // Fetch the product data from MongoDB
-    const products = await Product.find();
+    let mail = MailGenerator.generate(response)
 
-    // Render the 'product-details' template for each product
-    products.forEach(product => {
-      res.render('product-details', { product });
-    });
-  } catch (error) {
-    console.error('Error fetching product data:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+    let message = {
+        from : EMAIL,
+        to : userEmail,
+        subject: "Place Order",
+        html: mail
+    }
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+    transporter.sendMail(message).then(() => {
+        return res.status(201).json({
+            msg: "you should receive an email"
+        })
+    }).catch(error => {
+        return res.status(500).json({ error })
+    })
+
+    // res.status(201).json("getBill Successfully...!");
+}
 
 
-
-
+module.exports = {
+    signup,
+    getbill
+}
